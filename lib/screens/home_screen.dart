43cbb859115da2eeba544935872a65af78689754
Mike Mambwe <mikeapple056@gmail.com>
2024-10-19 01:27:14 +0200

    first and second def and the colorScheme

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,7 +13,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController _controller = TextEditingController();
   String _definition = '';
-   bool _isLoading = false;
+  String _secondDef = '';
+  bool _isLoading = false;
 
   //the method to fetch the definition
   Future<void> _fetchDefinition(String word) async {
@@ -25,6 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
       if (data.isNotEmpty) {
         setState(() {
           _definition = data[0]['meanings'][0]['definitions'][0]['definition'];
+          _secondDef =  data[0]['meanings'][1]['definitions'][1]['definition'];
         });
       } else {
         setState(() {
@@ -39,7 +41,11 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Center(child: Text('D E F I N E', style: TextStyle(color: Colors.white),)),
+        title: const Center(
+            child: Text(
+          'D E F I N E',
+          style: TextStyle(color: Colors.white),
+        )),
         backgroundColor: Colors.green[300],
       ),
       body: Padding(
@@ -49,22 +55,42 @@ class _HomeScreenState extends State<HomeScreen> {
             //the textfield
             TextField(
               controller: _controller,
-              decoration:const InputDecoration(
+              decoration: const InputDecoration(
                 hintText: 'Please enter word',
               ),
             ),
-           const SizedBox(height: 20,),
+            const SizedBox(
+              height: 20,
+            ),
             //the btn
             ElevatedButton(
-                onPressed: () {
-                  _fetchDefinition(_controller.text);
-                },
-                child: Text('Search'),),
-               const SizedBox(
+              onPressed: () {
+                _fetchDefinition(_controller.text);
+              },
+              child: Text('Search'),
+            ),
+            const SizedBox(
               height: 20,
             ),
             //the results
-            Text(_definition, style:const TextStyle(fontStyle:FontStyle.italic),),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  _definition,
+                  style: const TextStyle(fontStyle: FontStyle.italic),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                _secondDef == null ? Text('Second Definition')  : Text('')
+                ,
+               Text(
+                  _secondDef,
+                  style: const TextStyle(fontStyle: FontStyle.italic),
+                ),
+              ],
+            )
           ],
         ),
       ),
